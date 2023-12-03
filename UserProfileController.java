831ac612ef9eb564375fb10411e17d7db40007f8
qwer0asdf <151901935+qwer0asdf@users.noreply.github.com>
2023-12-03 14:51:46 +0900

    Update UserProfileController.java

--- UserProfileController.java
+++ UserProfileController.java
@@ -27,7 +27,6 @@ public class UserProfileController {
     @Scheduled(fixedRate = 5000) // 5초마다 이 메서드 실행 (필요에 따라 조정)
     public void fetchDataFromArduino() {
         // 아두이노에서 데이터를 가져와서 arduinoData를 업데이트하는 로직을 여기에 구현
-        // 예를 들어, 아두이노와 통신하기 위해 라이브러리나 API를 사용할 수 있다.
     }
 
     public static void main(String[] args) {
