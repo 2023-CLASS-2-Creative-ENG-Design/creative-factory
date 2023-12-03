faba7fbef2e48ad5ffc6297220d3e6b6c1f3e6e2
qwer0asdf <151901935+qwer0asdf@users.noreply.github.com>
2023-12-03 15:16:00 +0900

    Update UserProfileController.java

--- UserProfileController.java
+++ UserProfileController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 @RestController
 public class UserProfileController {
@@ -27,6 +30,16 @@ public class UserProfileController {
     @Scheduled(fixedRate = 5000) // 5초마다 이 메서드 실행 (필요에 따라 조정)
     public void fetchDataFromArduino() {
         // 아두이노에서 데이터를 가져와서 arduinoData를 업데이트하는 로직을 여기에 구현
+
+        // 현재 시간을 가져오기
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        // 시간을 문자열로 변환
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedTime = currentTime.format(formatter);
+
+        // arduinoData에 현재 시간 추가
+        arduinoData = formattedTime;
     }
 
     public static void main(String[] args) {
